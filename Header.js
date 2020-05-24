71037ae3a409d4d0d37eed304e029e5806e388b5
Alistair McCarten <44250062+alistairmccarten@users.noreply.github.com>
2020-05-24 12:54:20 -0600

    First Attempt at Transitioning to React
    Created multiple components in an attempt to transition the application to React

---
+++ Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header(){
+    return (
+        <div> 
+           <meta name="viewport" content="width=device-width intial-scale=1.0"/> 
+            <title> ChefSmart </title>
+           <script src="https://kit.fontawesome.com/bef64fdacc.js" crossorigin="anonymous"></script>
+           <link rel="stylesheet" href="styles.css"/> 
+           <h1> ChefSmart </h1> 
+        </div>
+    )
+}
+
+export default Header
---
+++ MainContent.js
@@ -0,0 +1,17 @@
+import React from "react" 
+
+function MainContent(){
+    return(
+        <div>
+            <div class="wrapper">
+            <div class="search-box">
+            <input type="text" placeholder="Enter your ingredient(s)?"/> 
+            <i class="fas fa-search"></i>
+            </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default MainContent
