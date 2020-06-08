6694ea49f664e617bc50004145dd23e084f9fb61
youngGaf <youngabuhafiz@gmail.com>
2020-06-08 04:26:43 +0100

    New commit adding database

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+require('dotenv/config');
 
+
+//Middlewares
+app.use('/', (req,res)=>{
+    res.send("Done and dusted");
+});
 //ROUTES
 app.get('/',(req,res) => {
     res.send("Hello world!")
 });
 
+//connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useUnifiedTopology: true, useNewUrlParser: true }, () =>
+    console.log("connected to DB")
+);
 
-//hown to listen to server
+//How to listen to server
 app.listen(3000);
