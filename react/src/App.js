a5664cf276b118f6d72025aac4bd99bd61e05b6a
Alistair McCarten <alistairmccarten@gmail.com>
2020-06-29 17:18:03 -0600

    Added Top Navigation Bar

--- react/src/App.js
+++ react/src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './App.css';
 import Wordmark from './components/Wordmark.js';
 import SearchBar from "./components/SearchBar.js";
+import SaveFeature from "./components/SaveFeature.js"
+import Login from "./components/Login.js"
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faHeart)
 
 
 function App() {
   return (
     <div className="App">
+        <Login/>
+        <SaveFeature />
         <Wordmark/> 
         <SearchBar/> 
     </div>
---
+++ react/src/components/SaveFeature.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function SaveFeature(){
+  return (
+  <div>
+    <p className="SaveFeature"> Save Feature Coming Soon </p>
+    <FontAwesomeIcon icon="heart" color="orangered" size="lg" />
+  </div>
+  )
+}
+
+export default SaveFeature 
