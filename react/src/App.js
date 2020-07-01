a9a44e29b29e25c6a474417eaf3329d976b3b4bc
Alistair McCarten <alistairmccarten@gmail.com>
2020-07-01 15:59:46 -0600

    Added search tags

--- react/src/App.js
+++ react/src/App.js
@@ -4,12 +4,13 @@ import Wordmark from './components/Wordmark.js';
 import SearchBar from "./components/SearchBar.js";
 import Navbar from "./components/Navbar.js"
 import RecipeCards from "./components/RecipeCards.js"
+import SearchTags from "./components/SearchTags.js"
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faHeart, faTimes } from '@fortawesome/free-solid-svg-icons'
 
-library.add(fab, faHeart)
+library.add(fab, faHeart, faTimes)
 
 
 function App() {
@@ -17,7 +18,8 @@ function App() {
     <div className="App">
         <Navbar/>
         <Wordmark/> 
-        <SearchBar/> 
+        <SearchBar/>
+        <SearchTags />  
         <RecipeCards />
     </div>
   );
